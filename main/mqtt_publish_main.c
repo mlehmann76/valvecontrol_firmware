8f37eee3b8b24aaa4b8805b4963592326c66354b
Marco Lehmann <marco.lehmann@freenet.de>
2018-06-26 21:47:09 +0200

    changed topic names

--- main/mqtt_publish_main.c
+++ main/mqtt_publish_main.c
@@ -92,11 +92,15 @@ const int CONNECTED_BIT = BIT0;
 #define MQTT_PORT 8883
 #define MQTT_BUF_SIZE 1000
 #define MQTT_WEBSOCKET 0 // 0=no 1=yes
-#define MQTT_SUB_MESSAGE "esp32/control1/#"
-#define MQTT_PUB_MESSAGE "esp32/control1/"
+
+#define MQTT_PUB_MESSAGE_FORMAT "esp32/%02X%02X/%s"
 
 static const char *TAG = "MQTTS";
 static const char* chanNames[] = {"channel0","channel1","channel2","channel3"};
+static const int maxChanIndex = sizeof(chanNames)/sizeof(chanNames[0]);
+
+static char mqtt_sub_msg[32] = {0};
+static char mqtt_pub_msg[32] = {0};
 
 /* subqueue for handling messages to gpio,
  * pubQueue for handling messages from gpio (autoOff) to mqtt */
@@ -118,7 +122,7 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
             /* send status of all avail channels */
 			queueData_t data = { 0, mStatus };
-			for (int i = 0; i < (sizeof(chanNames) / sizeof(chanNames[0])); i++) {
+			for (int i = 0; i < maxChanIndex; i++) {
 				data.chan = i;
 				if ( xQueueSend(subQueue, (void * ) &data,
 						(TickType_t ) 10) != pdPASS) {
@@ -126,7 +130,7 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 					ESP_LOGI(TAG, "subqueue post failure");
 				}
 			}
-			msg_id = esp_mqtt_client_subscribe(client, MQTT_SUB_MESSAGE, 1);
+			msg_id = esp_mqtt_client_subscribe(client, mqtt_sub_msg, 1);
 			ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
             break;
         case MQTT_EVENT_DISCONNECTED:
@@ -164,16 +168,19 @@ static void mqtt_message_handler(esp_mqtt_event_handle_t event) {
 	ESP_LOGI(TAG, "Topic received!: %.*s %.*s", event->topic_len,
 			 event->topic, event->data_len,	event->data);
 
-	if (event->topic_len > strlen(MQTT_SUB_MESSAGE)) {
-		pTopic = &event->topic[strlen(MQTT_SUB_MESSAGE)-1];
+	if (event->topic_len > strlen(mqtt_sub_msg)) {
+		pTopic = &event->topic[strlen(mqtt_sub_msg)-1];
 
-		ESP_LOGI(TAG, "%.*s", event->topic_len - strlen(MQTT_SUB_MESSAGE) + 1, pTopic);
+		ESP_LOGI(TAG, "%.*s", event->topic_len - strlen(mqtt_sub_msg) + 1, pTopic);
 
 		if (*pTopic != 0) {
 			chan = -1;
-			for (int i = 0; i < (sizeof(chanNames) / sizeof(chanNames[0]));
-					i++) {
-				if (strncmp(pTopic, chanNames[i], event->topic_len - strlen(MQTT_SUB_MESSAGE) + 1) == 0) {
+			for (int i = 0; i < maxChanIndex; i++) {
+				/* add /control/channelName to check for messages */
+				char buf[32];
+				snprintf(buf, sizeof(buf), "%s%s", "/control/", chanNames[i]);
+
+				if (strncmp(pTopic, buf, event->topic_len - strlen(mqtt_sub_msg) + 1) == 0) {
 					chan = i;
 					ESP_LOGI(TAG,"channel :%d found", i);
 					break;
@@ -276,7 +283,7 @@ static void initialise_wifi(void) {
 		xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true,
 				xTicksToWait);
 
-	} while ((xEventGroupGetBits(wifi_event_group) & CONNECTED_BIT) == 0);
+	} while (((xEventGroupGetBits(wifi_event_group) & CONNECTED_BIT) == 0)&&(gpio_get_level(0) != 0));
 }
 
 
@@ -285,7 +292,6 @@ void mqtt_task(void *pvParameters) {
 	while (1) {
 		// check if we have something to do
 		queueData_t rxData = { 0 };
-		const int maxChanIndex = sizeof(chanNames)/sizeof(chanNames[0]);
 
 		if ((client != NULL)
 				&& (xQueueReceive(pubQueue, &(rxData), (TickType_t ) 10))) {
@@ -298,14 +304,12 @@ void mqtt_task(void *pvParameters) {
 				char buf[255] = { 0 };
 				char payload[16] = { 0 };
 
-				snprintf(buf, sizeof(buf), "%s%s", MQTT_PUB_MESSAGE,
-						chanNames[chan]);
+				snprintf(buf, sizeof(buf), "%s%s", mqtt_pub_msg, chanNames[chan]);
 				snprintf(payload, sizeof(payload), "%s",
 						rxData.mode == mOn ? "son" : "soff");
 
 				ESP_LOGI(TAG, "publish %.*s : %.*s", strlen(buf), buf,
 						strlen(payload), payload);
-				//
 
 				msg_id = esp_mqtt_client_publish(client, buf, payload,
 						strlen(payload) + 1, 1, 0);
@@ -341,6 +345,13 @@ void app_main() {
 		ret = nvs_flash_init();
 	}
 	ESP_ERROR_CHECK(ret);
+
+	uint8_t mac[6] = {0};
+	esp_efuse_mac_get_default(mac);
+	snprintf(mqtt_sub_msg, sizeof(mqtt_sub_msg), MQTT_PUB_MESSAGE_FORMAT, mac[5],mac[4],"#");
+	snprintf(mqtt_pub_msg, sizeof(mqtt_pub_msg), MQTT_PUB_MESSAGE_FORMAT, mac[5],mac[4], "state/");
+
+	ESP_LOGI(TAG, "sub: %s, pub: %s", mqtt_sub_msg, mqtt_pub_msg);
 
 	initialise_wifi();
 
